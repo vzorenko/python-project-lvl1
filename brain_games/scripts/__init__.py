effd597803665a9e1f7731cf8cfe6bbf75c222f7
Vasiliy <services@zorenko.info>
2021-03-02 01:14:30 +0200

    New start

---
+++ brain_games/scripts/__init__.py
@@ -0,0 +1,8 @@
+#from brain_games import wellcome
+
+
+#def main():
+#    wellcome()
+
+#if __name__ == '__main__':
+#    main()
---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,14 @@
+#file brain_games.py
+
+
+
+
+def wellcome():
+    print('Welcome to the Brain Games!')
+
+def main():
+    wellcome()
+
+
+if __name__ == '__main__':
+    main()
