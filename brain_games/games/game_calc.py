7144859bf5f236ca97f5e10bf21fd9aaa0f020b2
Vasiliy Z <services@zorenko.info>
2021-03-04 22:12:47 +0200

    fix bad syntax

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -12,22 +12,19 @@ import random
 def game(max_steps=3):
     pprint('What is the result of the expression?')
     cur_step = 0
-    operations = [operator.mul, operator.add, operator.sub]
+    operations = [
+        {'operator_symbol': '*', 'func': operator.mul},
+        {'operator_symbol': '+', 'func': operator.add},
+        {'operator_symbol': '-', 'func': operator.sub}
+    ]
     while True:
         cur_step += 1
         cur_number1 = random.randrange(1, 10)
         cur_number2 = random.randrange(1, 10)
         cur_operation = random.choice(operations)
-        cur_result = cur_operation(cur_number1, cur_number2)
-        cur_operation_symbol = ''
-        if cur_operation is operator.mul:
-            cur_operation_symbol = '*'
-        if cur_operation is operator.add:
-            cur_operation_symbol = '+'
-        if cur_operation is operator.sub:
-            cur_operation_symbol = '-'
+        cur_result = cur_operation['func'](cur_number1, cur_number2)
 
-        print("Question: {} {} {}".format(cur_number1, cur_operation_symbol, cur_number2))
+        print("Question: {} {} {}".format(cur_number1, cur_operation['operator_symbol'], cur_number2))
 
         str_answer = prompt.string('Your answer: ')
         try:
