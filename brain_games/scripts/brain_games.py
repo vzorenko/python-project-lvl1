c1c6ab235c62c71ebb1b2a4592cf45b80c4c67d2
Василий <vasylzorenko@Great-Mac-9.local>
2021-03-04 19:27:53 +0200

    save current work version

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,20 +3,19 @@
 
 """Example project."""
 
-
 from brain_games.cli import welcome_user
 
 from pprint import pprint
 
 
-def wellcome():
+def welcome():
     """Doc string."""
     pprint('Welcome to the Brain Games!')
 
 
 def main():
     """Doc string."""
-    wellcome()
+    welcome()
     welcome_user()
 
 
